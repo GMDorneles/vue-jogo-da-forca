f489af4997238b4525a36db24e331f6afc54c51b
Gabrielly Maldonado <gabrielly.maldonadoo@gmail.com>
2023-03-02 23:58:11 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>Jogo da Forca WDEV</h1>
+  </div>
+</template>
+
+<script>
+import './css/global.css';
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
